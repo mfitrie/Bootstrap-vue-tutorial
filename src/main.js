e0fc9ff8f119e7833bf1af194ab58566d3107b5f
Fitrie <mfitrie78@gmail.com>
2022-09-25 23:50:20 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// // Important to add when use bootstrap-vue
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+// // Import Bootstrap and BootstrapVue CSS files (order is important)
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
+// // Important to add when use bootstrap-vue
+
 Vue.config.productionTip = false;
+
+// // Important to add when use bootstrap-vue
+// Vue.use(BootstrapVue);
+// Vue.use(IconsPlugin);
+// // Important to add when use bootstrap-vue
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!-- <div class="home"></div> -->
+  <b-container>
+    <b-row align-v="center">
+      <b-col md="3">
+        <b-card
+          title="This Is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
+            necessitatibus dicta minima facere earum qui alias sequi a enim.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
+        <b-card
+          title="This Is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
+            necessitatibus dicta minima facere earum qui alias sequi a enim.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
+        <b-card
+          title="This Is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
+            necessitatibus dicta minima facere earum qui alias sequi a enim.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
+        <b-card
+          title="This Is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
+            necessitatibus dicta minima facere earum qui alias sequi a enim.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
