c2fa1089121233cfc163459656d1884c936547e4
Fitrie <mfitrie78@gmail.com>
2022-09-27 18:26:48 +0800

    adding navbar and make other component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,90 +2,21 @@
   <!-- <div class="home"></div> -->
   <b-container>
     <b-row align-v="center">
-      <b-col md="3">
-        <b-card
-          title="This Is a Card"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-Info"
-          img-top
-          tag="article"
-          style="max-width: 20rem"
-          class="mb-2"
-        >
-          <b-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
-            necessitatibus dicta minima facere earum qui alias sequi a enim.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card
-          title="This Is a Card"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-Info"
-          img-top
-          tag="article"
-          style="max-width: 20rem"
-          class="mb-2"
-        >
-          <b-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
-            necessitatibus dicta minima facere earum qui alias sequi a enim.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card
-          title="This Is a Card"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-Info"
-          img-top
-          tag="article"
-          style="max-width: 20rem"
-          class="mb-2"
-        >
-          <b-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
-            necessitatibus dicta minima facere earum qui alias sequi a enima.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card
-          title="This Is a Card"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-Info"
-          img-top
-          tag="article"
-          style="max-width: 20rem"
-          class="mb-2"
-        >
-          <b-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            ea magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
-            necessitatibus dicta minima facere earum qui alias sequi a enim....
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
+      <JobCard />
+      <JobCard />
+      <JobCard />
+      <JobCard />
     </b-row>
   </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import JobCard from "../components/JobCard.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    JobCard,
+  },
 };
 </script>
