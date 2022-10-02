34dde3c18cba01e6ad122392d9981842ce707bce
Fitrie <mfitrie78@gmail.com>
2022-10-02 13:42:25 +0800

    making pagination

---
+++ src/components/JobCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-col>
+      <b-card
+        :title="name"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Job-Info"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2"
+        footer="Card footer"
+      >
+        <b-card-text
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ea
+          magni quia hic dolores iste officiis sit. Ullam, omnis vitae,
+          necessitatibus dicta minima facere earum qui alias sequi a enim.
+        </b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobCard",
+  props: ["name"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,21 @@
   <!-- <div class="home"></div> -->
   <b-container>
     <b-row align-v="center">
+      <JobCard v-for="job in displayJobs" :key="job.id" :name="job.name" />
+      <!-- <JobCard />
       <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
+      <JobCard /> -->
     </b-row>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input="paginate(currentPage)"
+    ></b-pagination>
   </b-container>
 </template>
 
@@ -17,6 +27,34 @@ export default {
   name: "HomeView",
   components: {
     JobCard,
+  },
+  data() {
+    return {
+      jobs: [],
+      displayJobs: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3,
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      // get json from public folder
+      const res = await fetch("jobs.json");
+      const val = await res.json();
+      this.jobs = val;
+      this.displayJobs = val.slice(0, 3);
+      this.rows = this.jobs.length;
+      console.log(val);
+    },
+    paginate(currentPage) {
+      // alert(currentPage);
+      const start = (currentPage - 1) * this.perPage;
+      this.displayJobs = this.jobs.slice(start, start + 3);
+    },
   },
 };
 </script>
